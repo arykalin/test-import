09876c1e82ab57919748205d61f56e042e7bf4d6
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-10-24 15:03:28 +0300

    Implementing list operation

--- plugin/pki/path_import_queue.go
+++ plugin/pki/path_import_queue.go
@@ -25,15 +25,7 @@ func pathImportQueue(b *backend) *framework.Path {
 
 func pathImportQueueList(b *backend) *framework.Path {
 	ret := &framework.Path{
-		//Pattern: `import-queue/(?P<queueRole>[0-9A-Fa-f]+)`,
-		//Fields: map[string]*framework.FieldSchema{
-		//	"queueRole": &framework.FieldSchema{
-		//		Type: framework.TypeString,
-		//		Description: `Queue role name`,
-		//	},
-		//},
-
-		Pattern: "import-queue/import/",
+		Pattern: "import-queue/",
 		Callbacks: map[logical.Operation]framework.OperationFunc{
 			logical.ListOperation: b.pathFetchImportQueueList,
 		},
@@ -41,25 +33,23 @@ func pathImportQueueList(b *backend) *framework.Path {
 		HelpSynopsis:    pathFetchHelpSyn,
 		HelpDescription: pathFetchHelpDesc,
 	}
-	//var fields map[string]*framework.FieldSchema
-	//fields["role"] = &framework.FieldSchema{
-	//	Type: framework.TypeString,
-	//	Description: `The desired role with configuration for this
-	//request`,
-	//	}
-	//ret.Fields = addNonCACommonFields(map[string]*framework.FieldSchema{})
 	return ret
 }
 
 func (b *backend) pathFetchImportQueueList(ctx context.Context, req *logical.Request, data *framework.FieldData) (response *logical.Response, retErr error) {
-	//roleName := data.Get("role").(string)
-	//log.Printf("Using role: %s", roleName)
-	//entries, err := req.Storage.List(ctx, "import-queue/"+data.Get("role").(string)+"/")
-	entries, err := req.Storage.List(ctx, "import-queue/"+data.Get("queueRole").(string)+"/")
+	roles, err := req.Storage.List(ctx, "import-queue/")
+	var entries []string
 	if err != nil {
 		return nil, err
 	}
-
+	for _, e := range roles {
+		log.Printf("Getting entry %s", e)
+		entry, err := req.Storage.List(ctx, "import-queue/"+e)
+		if err != nil {
+			return nil, err
+		}
+		entries = append(entries, entry...)
+	}
 	return logical.ListResponse(entries), nil
 }
 
