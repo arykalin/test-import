bd0836be443bff3a8228c0be0881f12e249cb8f2
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-10-24 14:39:51 +0300

    list is working in this case

--- plugin/pki/path_import_queue.go
+++ plugin/pki/path_import_queue.go
@@ -25,24 +25,37 @@ func pathImportQueue(b *backend) *framework.Path {
 
 func pathImportQueueList(b *backend) *framework.Path {
 	ret := &framework.Path{
-		Pattern: "import-queue-list/" + framework.GenericNameRegex("role"),
+		//Pattern: `import-queue/(?P<queueRole>[0-9A-Fa-f]+)`,
+		//Fields: map[string]*framework.FieldSchema{
+		//	"queueRole": &framework.FieldSchema{
+		//		Type: framework.TypeString,
+		//		Description: `Queue role name`,
+		//	},
+		//},
 
+		Pattern: "import-queue/import/",
 		Callbacks: map[logical.Operation]framework.OperationFunc{
-			//TODO: don't know why but ListOperation returning  "No value found at venafi-pki-import/import-queue-list/import/" error
-			logical.ReadOperation: b.pathFetchImportQueueList,
+			logical.ListOperation: b.pathFetchImportQueueList,
 		},
 
 		HelpSynopsis:    pathFetchHelpSyn,
 		HelpDescription: pathFetchHelpDesc,
 	}
-	ret.Fields = addNonCACommonFields(map[string]*framework.FieldSchema{})
+	//var fields map[string]*framework.FieldSchema
+	//fields["role"] = &framework.FieldSchema{
+	//	Type: framework.TypeString,
+	//	Description: `The desired role with configuration for this
+	//request`,
+	//	}
+	//ret.Fields = addNonCACommonFields(map[string]*framework.FieldSchema{})
 	return ret
 }
 
 func (b *backend) pathFetchImportQueueList(ctx context.Context, req *logical.Request, data *framework.FieldData) (response *logical.Response, retErr error) {
-	roleName := data.Get("role").(string)
-	log.Printf("Using role: %s", roleName)
-	entries, err := req.Storage.List(ctx, "import-queue/"+data.Get("role").(string)+"/")
+	//roleName := data.Get("role").(string)
+	//log.Printf("Using role: %s", roleName)
+	//entries, err := req.Storage.List(ctx, "import-queue/"+data.Get("role").(string)+"/")
+	entries, err := req.Storage.List(ctx, "import-queue/"+data.Get("queueRole").(string)+"/")
 	if err != nil {
 		return nil, err
 	}
